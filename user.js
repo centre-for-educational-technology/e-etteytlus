9b4d806ba7429482d25af5919b4b9a128f651e84
Priit Tammets <tammets@gmail.com>
2016-06-21 15:01:51 +0300

    Closes #17
    Hidden not removed

--- user.js
+++ user.js
@@ -5,36 +5,36 @@ function submit_user_info(e) {
 	if (!formVerify(e.target)) return;
 	var data = formData(e.target);
 	submissionData = data;
-	
+
 	var req = { "table" : "tests", "columns" : ["dateEnd"], "where" : {"code" : data.code} };
-	
+
 	ajax("db_select", req, function(r) {
 		var dateEnd = undefined;
 		try {
 			dateEnd = r[0][0].dateEnd;
 		} catch(e) {
 			show_message("Sellise koodiga etteütlust ei ole olemas", "Kontrolli koodi?");
-			navigate_timeout("#user_info", undefined, 6000);	
+			navigate_timeout("#user_info", undefined, 6000);
 			return;
 		}
-		
+
 		if (dateEnd < 10000) {
 			show_message("Etteütlus pole veel alanud!", "Kannatust");
 			navigate_timeout("#user_info", undefined, 4000);
 			return;
 		}
-		
+
 		if (dateEnd < unixTime()) {
 			show_message("Etteütlus on lõppenud.");
 			navigate_timeout("#user_info", undefined, 4000);
 			return;
 		}
-		
+
 		select("#s2ParticipantName").innerHTML = capitalize(data.firstname) + " " + capitalize(data.surname);
 		select("#s2TestCode").innerHTML = data.code;
-		select("#s2TimeRemaining").setAttribute("data-countdown-end", dateEnd);
+		// select("#s2TimeRemaining").setAttribute("data-countdown-end", dateEnd);
 		navigate("#dictation");
-		
+
 	}, function(r) {
 		show_message("Süsteemi viga", "Vabandame");
 		navigate_timeout("#user_info", undefined, 10000);
@@ -47,16 +47,16 @@ function submit_dictation(e) {
 	var data = formData(e.target);
 	submissionData["text"] = data.text;
 	submissionData["table"] = "submissions";
-	
+
 	show_message("Palun oodake", "Töötlen teksti");
-	ajax("db_insert", submissionData, function(r) {		
+	ajax("db_insert", submissionData, function(r) {
 		show_message("Etteütlus sooritatud!", "Täname osalemise eest.");
 	}, function(r) {
 		show_message("Süsteemi viga", "Vabandame");
 		navigate_timeout("#user_info", undefined, 10000);
 	});
-	
-	
+
+
 }
 
 //	************

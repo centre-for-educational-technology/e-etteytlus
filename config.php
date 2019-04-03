865d5d9b45625311a5623ab4a2df50b18d675640
Karl Aleksander <karl.leoste@gmail.com>
2019-04-03 15:39:05 +0300

    my setup

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php header('charset=utf-8');
 	define("db_settings_name", "eetteytlus");
 	define("db_settings_user", "root");
-	define("db_settings_password", "");
+	define("db_settings_password", "root");
 	define("db_settings_host", "localhost");
-	define("db_settings_port", "3306");
+	define("db_settings_port", "8889");
 	define("db_settings_charset", "utf8mb4");
 	define("db_settings_collate", "utf8mb4_unicode_ci");
 	
